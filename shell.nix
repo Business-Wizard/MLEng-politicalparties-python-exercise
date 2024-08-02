d243cb1d19e7e979d18fea3742b37754779d2726
Business-Wizard <49175856+Business-Wizard@users.noreply.github.com>
2024-08-02 09:05:30 -0600

    build: start vm if os is not Linux
    Signed-off-by: Business-Wizard <49175856+Business-Wizard@users.noreply.github.com>

--- shell.nix
+++ shell.nix
@@ -16,11 +16,19 @@ pkgs.mkShellNoCC {
 
   shellHook = ''
     if [ ! -d ".venv" ]; then
-      echo "Creating a virtual environment..."
+      echo -e "Creating a virtual environment..."
       rye sync
     fi
 
-    echo "Activating the virtual environment..."
+    echo -e "Activating the virtual environment..."
     source .venv/bin/activate || source .venv/bin/activate
+
+    # if os is linux, no podman vm is needed
+    if [ "$(uname)" != "Linux" ]; then
+      echo -e "Starting podman machine..."
+      podman machine start
+    fi
+
+    echo -e "\nNix-shell is ready!"
   '';
 }
