5d02199931fa724a2f806f56759ebc9bbe6b9d2d
Business-Wizard <49175856+Business-Wizard@users.noreply.github.com>
2024-08-02 13:30:35 -0600

    refactor: clarify test double type
    Signed-off-by: Business-Wizard <49175856+Business-Wizard@users.noreply.github.com>

--- tests/test_data_loader.py
+++ tests/test_data_loader.py
@@ -6,7 +6,7 @@ sys.path.append('src/text_loader')
 from loader import DataLoader
 
 @pytest.fixture
-def mock_df() -> pd.DataFrame:
+def stub_df() -> pd.DataFrame:
     data = {
         "Party": ['Republican', 'Democrat', 'Republican'],
         "Tweet": ['blah!23564 https://hello.com', 'bloh@123skdjfps', 1234567]
@@ -15,28 +15,26 @@ def mock_df() -> pd.DataFrame:
     return df
 
 @pytest.fixture
-def data_loader(mocker, mock_df):
-    mocker.patch('pandas.read_csv', return_value=mock_df)
+def data_loader(mocker, stub_df):
+    mocker.patch('pandas.read_csv', return_value=stub_df)
     return DataLoader()
 
-def test_load_data(mocker, mock_df):
+def test_load_data(mocker, stub_df):
     data_loader = DataLoader()
-    mocker.patch.object(data_loader, 'data',  mock_df)
+    mocker.patch.object(data_loader, 'data',  stub_df)
     data_loader.load_data()
     assert not data_loader.data.empty, "Data should not be empty after loading."
 
-def test_remove_characters(mock_df):
-    text = mock_df['Tweet'].iloc[1]
+def test_remove_characters(stub_df):
+    text = stub_df['Tweet'].iloc[1]
     cleaned_text = DataLoader.remove_characters(text)
     assert cleaned_text == "blohskdjfps"
 
-def test_clean_text(data_loader, mock_df):
-    text = mock_df['Tweet'].iloc[0]
+def test_clean_text(data_loader, stub_df):
+    text = stub_df['Tweet'].iloc[0]
     cleaned_text = data_loader.clean_text(text)
     assert cleaned_text == "blah"
 
-    text = mock_df['Tweet'].iloc[2]
+    text = stub_df['Tweet'].iloc[2]
     cleaned_text = data_loader.clean_text(text)
     assert cleaned_text == ""
-    
-
